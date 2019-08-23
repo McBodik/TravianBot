fbb2f40819332899a5558980dc30bca77d166ef1
McBodik <McBodik@gmail.com>
2019-08-23 10:10:43 +0300

    add village model

---
+++ src/com/mcbodik/travbot/villages/VillageModel.java
@@ -0,0 +1,13 @@
+package com.mcbodik.travbot.villages;
+
+import java.util.Queue;
+
+public class VillageModel {
+    private String name;
+    private int lumber;
+    private int clay;
+    private int iron;
+    private int crop;
+
+    private Queue<?> plannedBuildQueue;
+}
